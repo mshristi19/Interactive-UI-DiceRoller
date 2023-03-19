d8257362dd0a9e25e50a4e348ef4937a5db7a84d
mshristi19 <mshristi19@tbc.edu.np>
2023-03-19 13:22:46 +0545

    Added Extra dice with functionality

--- app/src/main/java/com/example/extradiceroller/MainActivity.kt
+++ app/src/main/java/com/example/extradiceroller/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.extradiceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstDice: TextView = findViewById(R.id.firstDice)
+        val secondDice: TextView = findViewById(R.id.secondDice)
+        val rollButton: Button = findViewById(R.id.buttonRoll)
+
+        rollButton.setOnClickListener{
+            val diceNumber = (1..6).random();
+            firstDice.text = "" +diceNumber;
+            val diceNumber2 = (1..6).random();
+            secondDice.text = "" +diceNumber2
+        }
     }
 }
